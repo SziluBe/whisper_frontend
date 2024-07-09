eb062dd599d8de1ec08b753a9b90ef72cc9edf6a
SziluBe <s2099359@ed.ac.uk>
2024-07-09 13:37:17 +0100

    working demo (only logs transcription to console)

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,29 @@ import { AudioRecorder } from 'react-audio-voice-recorder';
 
 export default function App() {
 
-  async function getData() {
+  function blobToBase64(blob : Blob) {
+    return new Promise((resolve, _) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  async function getData(audio_base64 : string, api_key : string) {
     try {
-      const response = await fetch('https://api.runpod.ai/v2/kpfxevjxqeqod5/runsync?=', {
+      console.log("audio base64: " + audio_base64);
+      const response = await fetch('https://api.runpod.ai/v2/{endpoint_id}/runsync?=', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'authorization': '...'
+          'authorization': api_key
         },
         // body: '{\n  "input": {\n    "model": "large-v3",\n    "audio_base64": "..."\n  }\n}',
         body: JSON.stringify({
           'input': {
             'model': 'large-v3',
-            'audio_base64': '...'
+            'audio_base64': audio_base64,
+            // 'audio': audio,
           }
         })
       });
@@ -26,18 +36,28 @@ export default function App() {
 
       const json = await response.json();
       console.log(json);
+      return json;
     } catch (error) {
       console.error(error.message);
     }
   }
   
-  const addAudioElement = (blob: Blob) => {
+  async function addAudioElement(blob: Blob) {
     const url = URL.createObjectURL(blob);
     const audio = document.createElement('audio');
     audio.src = url;
     audio.controls = true;
     document.body.appendChild(audio);
 
+    const audio_base64 = await blobToBase64(blob);
+
+    // slice at 35 to remove metadata at the start
+    const response = await getData(audio_base64.slice(35), "...");
+    const recognised_text = String(response.output.transcription);
+
+    console.log("recognised text: " + recognised_text);
+
+    // document.createElement()
   };
 
   return (
@@ -55,8 +75,9 @@ export default function App() {
           // sampleSize,
         }}
         onNotAllowedOrFound={(err) => console.table(err)}
-        // downloadOnSavePress={true}
+        downloadOnSavePress={true}
         downloadFileExtension="webm"
+        // downloadFileExtension="mp3"
         mediaRecorderOptions={{
           audioBitsPerSecond: 128000,
         }}
